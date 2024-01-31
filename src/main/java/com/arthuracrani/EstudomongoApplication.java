c48bfbd50468f5232c5002f9b505180cee079173
Arthur Acrani <arthuracrani12@gmail.com>
2024-01-30 21:05:50 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/arthuracrani/EstudomongoApplication.java
@@ -0,0 +1,13 @@
+package com.arthuracrani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudomongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudomongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthuracrani/EstudomongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthuracrani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudomongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
