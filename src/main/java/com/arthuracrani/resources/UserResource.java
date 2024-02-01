05c362cedd3cb704a9ed81a4fc941e1becaa75d0
Arthur Acrani <arthuracrani12@gmail.com>
2024-01-31 21:21:01 -0300

    Usando padrão DTO para retornar usuáraios e obtendo usuáario por id

--- src/main/java/com/arthuracrani/resources/UserResource.java
+++ src/main/java/com/arthuracrani/resources/UserResource.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arthuracrani.domain.User;
+import com.arthuracrani.dto.UserDTO;
 import com.arthuracrani.services.UserService;
 
 
@@ -26,5 +28,10 @@ public class UserResource {
  	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+ 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
--- src/main/java/com/arthuracrani/services/UserService.java
+++ src/main/java/com/arthuracrani/services/UserService.java
@@ -1,11 +1,13 @@
 package com.arthuracrani.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.arthuracrani.domain.User;
+import com.arthuracrani.exception.ObjectNotFoundException;
 import com.arthuracrani.repository.UserRepository;
 
 @Service
@@ -16,5 +18,9 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
